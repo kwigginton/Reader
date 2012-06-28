dd0ae57a795434b144397ddeacf6b926d17aaa76
Kenneth Wigginton <Hailstorm350@gmail.com>
2012-06-27 20:44:38 -0700

    alpha ready

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,5 +2,7 @@ class AdminController < ApplicationController
   
   def index
     @total_users = User.count
+    @total_feeds = Feed.count
+    @total_subscriptions = Subscription.count
   end
 end
